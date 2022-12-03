f0389e6c3a00bd3c428eaf412a5fdbdb83fa641f
mahesh jamdade <maheshmn121@gmail.com>
2022-12-03 11:58:57 -0500

    update libraries and fix deprecation

--- app/src/main/java/edu/umassd/traffictracker/MainActivity.java
+++ app/src/main/java/edu/umassd/traffictracker/MainActivity.java
@@ -1,5 +1,6 @@
 package edu.umassd.traffictracker;
 
+import android.Manifest;
 import android.app.ActivityManager;
 import android.app.AlertDialog;
 import android.app.Dialog;
@@ -10,18 +11,20 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.ServiceConnection;
 import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
 import android.location.LocationManager;
 import android.net.Uri;
 import android.os.IBinder;
 import android.preference.PreferenceManager;
-import android.support.v4.app.FragmentActivity;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
@@ -33,12 +36,13 @@ import com.google.android.gms.location.GeofencingRequest;
 import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.*;
 import com.google.android.gms.maps.model.*;
+import com.google.android.gms.maps.model.LatLng;
 
 
 /**
  * Starting point for the app. The app first connects to the GooglePlay services (Which provides access to GPS, Activity Monitor etc.)
  */
-public class MainActivity extends AppCompatActivity implements GoogleApiClient.ConnectionCallbacks,GoogleApiClient.OnConnectionFailedListener, ResultCallback, OnMapReadyCallback {
+public class MainActivity extends AppCompatActivity implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener, ResultCallback, OnMapReadyCallback {
     public Geofence mGeofence;
     public GeofencingRequest gfEnter;
     public PendingIntent mGeofencePendingIntent, activityHandlerPI;
@@ -50,7 +54,7 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.C
     String TAG = "MainActivity";
     boolean DEBUG = true;
     static boolean geofenceRunning = false;
-    public Intent intent,activityHandler;
+    public Intent intent, activityHandler;
 
     final Context c = this;
     public static Context context;
@@ -73,7 +77,7 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.C
         public void onServiceDisconnected(ComponentName arg0) {
             mBound = false;
         }
-        };
+    };
 
 
     /**
@@ -84,7 +88,7 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.C
      */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        if(DEBUG)Log.e(TAG, "OnCreate");
+        if (DEBUG) Log.e(TAG, "OnCreate");
         super.onCreate(savedInstanceState);
         //Check if app is started for the first time
         boolean ftc = firstTimeCheck();
@@ -97,20 +101,18 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.C
                 .addApi(ActivityRecognition.API).build();
 
         //Check if background service is already running (For cases when the app is closed and relaunched)
-        if (isMyServiceRunning(GeofenceTransitionsIntentService.class) && !ftc){
+        if (isMyServiceRunning(GeofenceTransitionsIntentService.class) && !ftc) {
             //If the geofencetransition is running already show status
-            if(DEBUG)Log.e(TAG, "geofencetransition IS running!");
+            if (DEBUG) Log.e(TAG, "geofencetransition IS running!");
             intent = new Intent(this, GeofenceTransitionsIntentService.class);
 
             setContentView(R.layout.activity_main);
             mGoogleApiClient.connect();
-        }
-        else if (ftc){
+        } else if (ftc) {
             //If app is started for the first time, show settings
             showIRB();
-        }
-        else {
-            if(DEBUG)Log.e(TAG, "geofencetransition is not running");
+        } else {
+            if (DEBUG) Log.e(TAG, "geofencetransition is not running");
             initialize();
             //Putting google map on app
 
@@ -118,7 +120,7 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.C
 
         //Code to display google map of the campus instead of boring buttons
         MapFragment mapFragment = (MapFragment) getFragmentManager().findFragmentById(R.id.map);
-        if(mapFragment != null)
+        if (mapFragment != null)
             mapFragment.getMapAsync(this);
     }
 
@@ -126,7 +128,7 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.C
      * If background service is NOT running and the app has NOT been started for the first time,
      * This function is run to initilize the different components of the app
      */
-    public void initialize(){
+    public void initialize() {
         //geofencetransition is not running, start it here
         setContentView(R.layout.activity_main);
         //Build the geofence
@@ -147,9 +149,9 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.C
      * THis function is triggered when the app is destroyed. Unbinds itself from the background service
      */
     @Override
-    protected void onDestroy(){
-        if(DEBUG)Log.e(TAG, "OnDestroy");
-        if(mBound) {
+    protected void onDestroy() {
+        if (DEBUG) Log.e(TAG, "OnDestroy");
+        if (mBound) {
             unbindService(mConnection);
             mBound = false;
         }
@@ -164,9 +166,19 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.C
     @Override
     public void onConnected(Bundle arg0) {
         // Once connected with google api, Add the geofence
-        if(DEBUG)Log.e(TAG, "Connected to API");
-        if(!geofenceRunning) {
-            if(DEBUG)Log.e(TAG, "Adding Geofences");
+        if (DEBUG) Log.e(TAG, "Connected to API");
+        if (!geofenceRunning) {
+            if (DEBUG) Log.e(TAG, "Adding Geofences");
+            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                // TODO: Consider calling
+                //    ActivityCompat#requestPermissions
+                // here to request the missing permissions, and then overriding
+                //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                //                                          int[] grantResults)
+                // to handle the case where the user grants the permission. See the documentation
+                // for ActivityCompat#requestPermissions for more details.
+                return;
+            }
             LocationServices.GeofencingApi.addGeofences(
                     mGoogleApiClient,
                     gfEnter,
@@ -174,26 +186,26 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.C
             ).setResultCallback(this);
             geofenceRunning = true;
         }
-        if(DEBUG)Log.e(TAG, "Starting activity detection");
+        if (DEBUG) Log.e(TAG, "Starting activity detection");
         /*Activity detection API*/
         activityHandler = new Intent(this, ActivityHandler.class);
         activityHandlerPI = PendingIntent.getService(this, 1, activityHandler, PendingIntent.FLAG_UPDATE_CURRENT);
 
         //Requests activity updates every x ms currently 5000 ms
-        ActivityRecognition.ActivityRecognitionApi.requestActivityUpdates(mGoogleApiClient,5000,activityHandlerPI);
+        ActivityRecognition.ActivityRecognitionApi.requestActivityUpdates(mGoogleApiClient, 5000, activityHandlerPI);
 
     }
 
     @Override
-    public void onResult(Result r){
+    public void onResult(Result r) {
         //Shows the result of connecting to the Google Play API (
         //TODO: Can fail, need to add notification to turn on GPS
-        if(DEBUG)Log.e(TAG, "OnResult : " + r.toString());
+        if (DEBUG) Log.e(TAG, "OnResult : " + r.toString());
     }
 
     @Override
     public void onConnectionFailed(ConnectionResult result) {
-        if(DEBUG)Log.i(TAG, "Connection failed: ConnectionResult.getErrorCode() = "
+        if (DEBUG) Log.i(TAG, "Connection failed: ConnectionResult.getErrorCode() = "
                 + result.getErrorCode());
     }
 
@@ -207,9 +219,9 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.C
      * Checks if GPS is enabled. If not, ask user to activate it.
      * If user does not activate GPS co-ordinates will be coarse.
      */
-    public void checkGPS(){
+    public void checkGPS() {
         locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
-        if( !locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER) ) {
+        if (!locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
             AlertDialog.Builder builder = new AlertDialog.Builder(this);
             builder.setTitle("GPS not enabled");  // GPS not found
             builder.setMessage("Would you like to enable GPS?"); // Want to enable?
@@ -228,7 +240,7 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.C
      * Method that handles connection to googleAPI
      * @param intent
      */
-    public void connectGoogleApiClient(Intent intent){
+    public void connectGoogleApiClient(Intent intent) {
         mGeofencePendingIntent = PendingIntent.getService(this, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
 
         //Connecting to Google API
@@ -240,7 +252,7 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.C
      * Method to build GeoFence at the center of UMass Dartmouth with a 1000 meter radius
      * Set up to trigger when the user enters or exits the geofence
      */
-    public void buildGeofence(){
+    public void buildGeofence() {
         mGeofence = new Geofence.Builder()
                 .setRequestId("UmassDartmouth")
                 .setCircularRegion(41.628931, -71.006228, 1000)
@@ -259,8 +271,8 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.C
      * Remove geofence when exiting the app. The geofence has to be explicitly removed or it will
      * still trigeer when the user is not running the app (or the background process is not running)
      */
-    public void removeGeofence(){
-        if(DEBUG)Log.e(TAG,"Removing geofence");
+    public void removeGeofence() {
+        if (DEBUG) Log.e(TAG, "Removing geofence");
         mGeofencePendingIntent = PendingIntent.getService(this, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
 
         LocationServices.GeofencingApi.removeGeofences(
@@ -269,6 +281,16 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.C
                 mGeofencePendingIntent
         ).setResultCallback(this); // Result processed in onResult().
 
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACTIVITY_RECOGNITION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
         ActivityRecognition.ActivityRecognitionApi.removeActivityUpdates(mGoogleApiClient, activityHandlerPI);
     }
 
@@ -276,13 +298,13 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.C
      * Performs a first time user check. If first time user, displays IRB form otherwise start app
      * @return
      */
-    public boolean firstTimeCheck(){
-        boolean result,irb;
+    public boolean firstTimeCheck() {
+        boolean result, irb;
         //Check preferences for first time run
         SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
         result = prefs.getBoolean("first_time", true);
-        irb = prefs.getBoolean("irbAccepted",false);
-        return result&&!irb;
+        irb = prefs.getBoolean("irbAccepted", false);
+        return result && !irb;
     }
 
     /**
@@ -354,14 +376,12 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.C
             Intent intent = new Intent(this, Settings.class);
             startActivity(intent);
             return true;
-        }
-        else if(id==R.id.action_sendfeedback){
+        } else if (id == R.id.action_sendfeedback) {
             Intent intent = new Intent(Intent.ACTION_VIEW);
             Uri data = Uri.parse("mailto:vshekar@umassd.edu?subject=Traffic App Feedback&body=");
             intent.setData(data);
             startActivity(intent);
-        }
-        else if(id==R.id.action_exit){
+        } else if (id == R.id.action_exit) {
             String title = "Exit";
             String message = "Are you sure you want to exit?\nYou will stop collecting data if you do";
             AlertDialog.Builder builder = new AlertDialog.Builder(c)
@@ -369,12 +389,12 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.C
                     .setMessage(message)
                     .setCancelable(false)
                     .setPositiveButton("Exit",
-                            new Dialog.OnClickListener(){
+                            new Dialog.OnClickListener() {
                                 @Override
                                 public void onClick(
-                                        DialogInterface dialogInterface, int i){
+                                        DialogInterface dialogInterface, int i) {
                                     stopService(intent);
-                                    if(mBound) {
+                                    if (mBound) {
                                         unbindService(mConnection);
                                         mBound = false;
                                     }
@@ -409,7 +429,7 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.C
         LatLng[] Lot = new LatLng[16];
         String[] lotName = new String[16];
 
-        LatLng umassD = new LatLng(41.628931,-71.006228);
+        LatLng umassD = new LatLng(41.628931, -71.006228);
         Lot[0] = new LatLng(41.631527, -71.004978);
         lotName[0] = "Lot 1";
         Lot[1] = new LatLng(41.631263, -71.003897);
@@ -443,6 +463,16 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.C
         Lot[15] = new LatLng(41.631331, -71.008515);
         lotName[15] = "Lot 17";
 
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
         map.setMyLocationEnabled(true);
         map.moveCamera(CameraUpdateFactory.newLatLngZoom(umassD, 15));
 
